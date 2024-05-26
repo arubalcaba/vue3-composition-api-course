d5445b64ecefb330cabd17c136072d1ac316311f
Andrew Rubalcaba <arubalcaba@806technologies.com>
2024-05-26 18:29:04 -0500

    Completed Section 3 Lesson 30
    these changes are related to completing Section 3 Lesson 30 of the course

--- src/components/PostWriter.vue
+++ src/components/PostWriter.vue
@@ -1,13 +1,38 @@
 <script lang="ts" setup>
 import { TimelinePost } from '../posts'
-import { ref, defineProps } from 'vue'
+import { ref, defineProps, watch } from 'vue'
+import { marked } from 'marked'
+
 const props = defineProps<{
     post: TimelinePost
 }>()
 
 const title = ref(props.post.title)
 const contentEditable = ref<HTMLDivElement>()
+const html = ref('')
 const content = ref(props.post.markdown)
+
+// watchEffect(() => {
+//     marked.parse(content.value, (err, result) => {
+//         if (err) {
+//             console.error(err)
+//             return
+//         }
+//         html.value = result
+//     })
+// })
+
+watch(content, (newContent) => {
+        marked.parse(newContent, (err, result) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        html.value = result
+    })
+},{ immediate: true })
+
+
 
 const handleInput = () => {
     content.value = contentEditable.value?.innerText || ''
@@ -32,7 +57,7 @@ const handleInput = () => {
             </div>
         </div>
         <div class="column">
-            {{ content }}
+            <div v-html="html"></div>
         </div>
     </div>
 </template>

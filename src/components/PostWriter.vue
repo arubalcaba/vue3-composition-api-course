12281ab5fbb5851ff7bfdbacd2fa2bfe9b9574a8
Andrew Rubalcaba <arubalcaba@806technologies.com>
2024-05-27 16:15:58 -0500

    Section 3 Lesson 33 - Added Debounce for delaying update to html content
    Section 3 Lesson 33 - Added Debounce for delaying update to html content

--- src/components/PostWriter.vue
+++ src/components/PostWriter.vue
@@ -3,6 +3,7 @@ import { TimelinePost } from '../posts'
 import { ref, defineProps, watch, onMounted } from 'vue'
 import { marked } from 'marked'
 import hljs from 'highlight.js'
+import { debounce } from 'lodash';
 
 const props = defineProps<{
     post: TimelinePost
@@ -29,8 +30,8 @@ onMounted(() => {
     }
 })
 
-watch(content, (newContent) => {
-        marked.parse(newContent,{
+function parseHtml(markdown: string) {
+    marked.parse(markdown,{
             gfm: true,
             breaks: true,
             highlight: (code) => {
@@ -40,7 +41,12 @@ watch(content, (newContent) => {
             console.log('result', result)
             html.value = result
         })
-},{ immediate: true })
+}
+
+watch(content, debounce((newContent) => {
+       parseHtml(newContent)
+},250),{ immediate: true }
+)
 
 
 
